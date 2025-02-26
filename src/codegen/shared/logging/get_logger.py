efe3d17b832c6e07f24a38d20f1b48f59c6b5361
Carol Jung <cjung@codegen.com>
2025-02-26 15:42:20 -0800

    dedupe logger handlers

--- src/codegen/shared/logging/get_logger.py
+++ src/codegen/shared/logging/get_logger.py
@@ -4,29 +4,31 @@ import colorlog
 
 
 def get_colored_logger(name: str) -> logging.Logger:
-    handler = colorlog.StreamHandler()
-    handler.setFormatter(
-        colorlog.ColoredFormatter(
-            "%(white)s%(asctime)s - %(name)s - %(log_color)s%(levelname)s%(reset)s%(white)s - %(message_log_color)s%(message)s",
-            log_colors={
+    formatter = colorlog.ColoredFormatter(
+        "%(white)s%(asctime)s - %(name)s - %(log_color)s%(levelname)s%(reset)s%(white)s - %(message_log_color)s%(message)s",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+        secondary_log_colors={
+            "message": {
                 "DEBUG": "cyan",
-                "INFO": "green",
+                "INFO": "blue",
                 "WARNING": "yellow",
                 "ERROR": "red",
                 "CRITICAL": "red,bg_white",
-            },
-            secondary_log_colors={
-                "message": {
-                    "DEBUG": "cyan",
-                    "INFO": "blue",
-                    "WARNING": "yellow",
-                    "ERROR": "red",
-                    "CRITICAL": "red,bg_white",
-                }
-            },
-        )
+            }
+        },
     )
-
     logger = logging.getLogger(name)
-    logger.addHandler(handler)
+    if logger.hasHandlers():
+        for h in logger.handlers:
+            h.setFormatter(formatter)
+    else:
+        handler = colorlog.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
