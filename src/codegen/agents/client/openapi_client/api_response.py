46e1537136c07596a5a1856ff0f54e7468978a00
Rushil Patel <rpatel@codegen.com>
2025-04-08 12:29:07 -0700

    fix: use dict instead of mapping type

--- src/codegen/agents/client/openapi_client/api_response.py
+++ src/codegen/agents/client/openapi_client/api_response.py
@@ -2,12 +2,9 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Generic, TypeVar
+from typing import Generic, TypeVar
 
 from pydantic import BaseModel, Field, StrictBytes, StrictInt
-
-if TYPE_CHECKING:
-    from collections.abc import Mapping
 
 T = TypeVar("T")
 
@@ -16,7 +13,7 @@ class ApiResponse(BaseModel, Generic[T]):
     """API response object"""
 
     status_code: StrictInt = Field(description="HTTP status code")
-    headers: Mapping[str, str] | None = Field(None, description="HTTP headers")
+    headers: dict[str, str] | None = Field(None, description="HTTP headers")
     data: T = Field(description="Deserialized data given the data type")
     raw_data: StrictBytes = Field(description="Raw data (HTTP response body)")
 
