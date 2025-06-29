29ac6d0299e14fe400b89b9fdb4f8ae3f3e3535a
codegen-team <135641899+codegen-team@users.noreply.github.com>
2025-06-29 19:53:28 +0000

    Automated pre-commit update

--- src/codegen/__init__.py
+++ src/codegen/__init__.py
@@ -16,5 +16,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.55.8.dev37+ge81a4c8b"
-__version_tuple__ = version_tuple = (0, 55, 8, "dev37", "ge81a4c8b")
+__version__ = version = "0.55.8.dev38+gbdc2aaba"
+__version_tuple__ = version_tuple = (0, 55, 8, "dev38", "gbdc2aaba")
